7eb046b010fec36e5311068bf00030fbdc5157e9
Fqruk <Omerfarukusta13@gmail.com>
2023-10-09 03:35:09 +0300

    api çekildi

--- myapp/src/Components/WeatherApp/WeatherApp.jsx
+++ myapp/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 import './WeatherApp.sass'
 import Container from 'react-bootstrap/Container';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { TiWeatherShower,TiWeatherDownpour } from 'react-icons/ti';
+import { TiWeatherShower, TiWeatherDownpour } from 'react-icons/ti';
 
 function WeatherApp() {
+
+  const key = "861d864a8e24788a2f765481015907bd";
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}&units=metric`);
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  } , [])
+
   return (
     <div className="app">
       <Container>
@@ -26,45 +42,45 @@ function WeatherApp() {
         <div className="weather-section">
           <div className="hour">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-          <h4 style={{marginTop:'1rem'}}>12:00 PM</h4>
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>12:00 PM</h4>
           </div>
           <div className="hour">
-          <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-          <h4 style={{marginTop:'1rem'}}>12:00 PM</h4>
+            <h2>21°C</h2>
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>12:00 PM</h4>
           </div>
           <div className="hour">
-          <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-          <h4 style={{marginTop:'1rem'}}>12:00 PM</h4>
+            <h2>21°C</h2>
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>12:00 PM</h4>
           </div>
         </div>
         <div className="weather-section">
           <div className="day">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-            <h4 style={{marginTop:'1rem'}}>Pzt</h4>    
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>Pzt</h4>
           </div>
           <div className="day">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-            <h4 style={{marginTop:'1rem'}}>Pzt</h4>    
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>Pzt</h4>
           </div>
           <div className="day">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-            <h4 style={{marginTop:'1rem'}}>Pzt</h4>    
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>Pzt</h4>
           </div>
           <div className="day">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-            <h4 style={{marginTop:'1rem'}}>Pzt</h4>    
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>Pzt</h4>
           </div>
           <div className="day">
             <h2>21°C</h2>
-            <TiWeatherDownpour style={{fontSize:'3rem'}}></TiWeatherDownpour>
-            <h4 style={{marginTop:'1rem'}}>Pzt</h4>    
+            <TiWeatherDownpour style={{ fontSize: '3rem' }}></TiWeatherDownpour>
+            <h4 style={{ marginTop: '1rem' }}>Pzt</h4>
           </div>
         </div>
       </div>
